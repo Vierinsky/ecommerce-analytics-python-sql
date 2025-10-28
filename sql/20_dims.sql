e2f6888748470756aebf55da4e4a2938b029ad4b
Felipe <felipe.viera@mayor.cl>
2025-10-28 11:05:40 -0300

    Creating core.dim_customer

--- sql/20_dims.sql
+++ sql/20_dims.sql
@@ -1,3 +1,4 @@
+-- dim_calendar table
 CREATE TABLE IF NOT EXISTS core.dim_calendar (
     calendar_sk SERIAL PRIMARY KEY,
     date DATE UNIQUE NOT NULL,
@@ -29,3 +30,22 @@ SELECT
     EXTRACT(YEAR FROM date)::int
 FROM series
 ON CONFLICT (date) DO NOTHING;
+
+-- dim_customer table
+CREATE TABLE IF NOT EXISTS core.dim_customer(
+    customer_sk SERIAL PRIMARY KEY,
+    customer_id TEXT UNIQUE NOT NULL,
+    customer_city TEXT,
+    customer_state TEXT
+);
+
+-- Normalization and populating of dim_customer table
+INSERT INTO core.dim_customer (customer_id, customer_city, customer_state)
+SELECT DISTINCT
+    c.customer_id,
+    TRIM(LOWER(c.customer_city)) AS customer_city,
+    TRIM(UPPER(c.customer_state)) AS customer_state
+FROM staging.customers_raw c
+ON CONFLICT (customer_id) DO NOTHING;
+
+-- (Optional) core.dim_product
